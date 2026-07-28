a744df503efd165d1083e72d082cf49663dc92da
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-28 07:46:14 +0600

    add the clerk authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { ClerkProvider } from '@clerk/react'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ClerkProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
   </BrowserRouter>,
 )
