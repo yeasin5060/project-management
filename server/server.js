46096f3bccd6341408a045774b46c5b0e40e9282
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-29 09:02:34 +0600

    add the neon database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express();
 
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.use( '/', ( req , res)=> res.send('Server is live!'));
 
