e8564dd1863ffe4f8ce28a3c70160de7746229a8
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-30 23:12:53 +0600

    complete the createuser function inngest

---
+++ server/inngest/index.js
@@ -0,0 +1,28 @@
+import { Inngest } from "inngest";
+import { prisma } from "../src/db.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "project-management" });
+
+// ✅ inngest function to the create a user
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+        const {data} = event
+        await prisma.user.create({
+            data : {
+                id : data.id,
+                email: data?.email_addresses?.[0]?.email_address,
+                name: `${data?.first_name || ""} ${data?.last_name || ""}`.trim(),
+                image: data?.image_url,
+            }
+        })
+  }
+);
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
+import {serve} from 'inngest/express';
+import { inngest , functions} from './inngest/index.js';
 
 
 const app = express();
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.use( '/', ( req , res)=> res.send('Server is live!'));
+app.use('/api/inngest', serve({client: inngest, functions}));
 
 const PORT = process.env.PORT || 5000 ;
 
