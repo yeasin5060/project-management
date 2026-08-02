dfe19b687fe4c1d476e4a5870bac87e83bb503a2
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-02 08:22:46 +0600

    install neondatabase/serverless

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import {serve} from 'inngest/express';
-import { inngest , functions} from './inngest/index.js';
+import {inngest,functions} from './inngest/index.js'
 
 
 const app = express();
