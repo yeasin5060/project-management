105462e5901120c4e7e95d16db0c182146da8e68
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-03 19:34:31 +0600

    change the server.js file

--- server/server.js
+++ server/server.js
@@ -2,19 +2,25 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
-import {serve} from 'inngest/express';
-import {inngest,functions} from './inngest/index.js'
+import { functions, inngest } from './inngest/index.js';
+import { serve } from 'inngest/express'
+
+
 
 
 const app = express();
 
 app.use(cors());
 
-app.use('/api/inngest', serve({client: inngest, functions}));
 
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {res.send("Server is live!");});
+app.use('/api/inngest',serve({
+    client: inngest,
+    functions,
+  })
+);
 
 const PORT = process.env.PORT || 5000 ;
 
