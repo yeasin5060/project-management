35c24b1830c5fa74c96b43bb197ee1145d74a21b
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-07 16:53:28 +0600

    start the addMember controller

--- server/controllers/workspace.controllers.js
+++ server/controllers/workspace.controllers.js
@@ -27,3 +27,45 @@ export const getUserWorkSpaces = async (req , res) => {
         res.status(500).json({message : error.code || error,message});
     }
 }
+
+//add member to worksapces
+
+export const addMember = async (req ,res) => {
+    try {
+        const {userId} = await req.auth();
+
+        const {email, role , workspaceId , message} = req.body;
+
+        //check if user exists
+        const user = await prisma.user.findUnique({where : {email}});
+        if(!user) {
+            return res.status(404).json({message : "User not found"});
+        }
+
+        if(!workspaceId || !role) {
+            return res.status(400).json({message : "Missing required parameters"});
+        }
+
+        if(!["ADMIN" , "MEMBER"].includes(role)) {
+            return res.status(400).json({message : "Invalid role"});
+        }
+            //fetch workspace
+        const workspace = await prisma.workspace.findUnique({
+            where : {id : workspaceId},
+            include : {members : true}
+        });
+
+        if(!workspace) {
+            return res.status(404).json({message : "Workspace not found"});
+        }
+
+        //check creator has admin role
+        if(!workspace.members.find((member) => member.userId === userId && member.role === "ADMIN")){
+            return res.status(401).json({message : "You do not have admin privileges"});
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message : error.code || error,message});
+    }
+}
